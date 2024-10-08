b8e5d7c810801f9eeaa1c6a0230a8683028ac91d
Michele Lanza <michele_lanza1@hotmail.com>
2024-10-07 22:22:24 -0400

    añadiendo capa de enterprise

---
+++ EnterpriseLayer/VideoGameConsole.cs
@@ -0,0 +1,12 @@
+﻿namespace EnterpriseLayer
+{
+    public class VideoGameConsole
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime LaunchDate { get; set; }
+
+        public bool IsRetro() => LaunchDate.Year <= 2005;
+    }
+}
